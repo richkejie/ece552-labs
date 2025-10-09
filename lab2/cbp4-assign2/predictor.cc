bc66033b2917f9d20e8bd969221c6fd7111a66bf
Wu  Richard <wuricha8@ug170.eecg.toronto.edu>
2025-10-09 00:22:54 -0400

    added implementation of 2-bit sat counter predictor

--- lab2/cbp4-assign2/predictor.cc
+++ lab2/cbp4-assign2/predictor.cc
@@ -4,17 +4,53 @@
 // 2bitsat
 /////////////////////////////////////////////////////////////
 
+#define TWO_BIT_SAT_TABLE_BITS 8192
+#define TEN_LSB_MASK 0x3FF
+int *state_table_2bitsat;
 void InitPredictor_2bitsat() {
-
+  /* 
+    0 - strong NT
+    1 - weak NT
+    2 - weak T
+    3 - strong T
+  */
+  /*
+    represent each counter as a byte (so 8192/8 = 1024 counters)
+    easier to implement indexing in this way...
+  */
+  state_table_2bitsat = (int*)malloc(TWO_BIT_SAT_TABLE_BITS/8*sizeof(int)); 
+  int i = 0;
+  /*
+    init all counters to weak not taken
+  */
+  while(i < TWO_BIT_SAT_TABLE_BITS/8){
+    *(state_table_2bitsat + i) = 1;
+    i++;
+  }
 }
 
 bool GetPrediction_2bitsat(UINT32 PC) {
-
-  return TAKEN;
+  /*
+    get the 10 LSB of PC to index state table
+    2^10 = 1024 possible indices
+  */
+  int index = (int)(PC & (UINT32)TEN_LSB_MASK);
+  int state = *(state_table_2bitsat + index);
+  return state <= 1 ? NOT_TAKEN : TAKEN;
 }
 
 void UpdatePredictor_2bitsat(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
-
+  /*
+    get the state of counter
+  */
+  int index = (int)(PC & (UINT32)TEN_LSB_MASK);
+  int state = *(state_table_2bitsat + index);
+  /*
+    update the counter
+  */
+  if(resolveDir == TAKEN) state = ((state + 1) > 3 ? 3 : (state + 1));
+  else state = ((state - 1) < 0 ? 0 : (state - 1));
+  *(state_table_2bitsat + index) = state;
 }
 
 /////////////////////////////////////////////////////////////
