03b296a1d0fa69da98ae57d6c148d836a062680b
Wu  Richard <wuricha8@ug170.eecg.toronto.edu>
2025-10-22 20:14:08 -0400

    dispatch_to_issue + print statements for debugging + some other stops for debugging (see cycle limit in runTomasulo)

--- lab3/simplesim-3.0d-assig3/tomasulo.c
+++ lab3/simplesim-3.0d-assig3/tomasulo.c
@@ -25,13 +25,13 @@
 
 #define INSTR_QUEUE_SIZE         16
 
-#define RESERV_INT_SIZE    4
-#define RESERV_FP_SIZE     2
-#define FU_INT_SIZE        2
+#define RESERV_INT_SIZE    5
+#define RESERV_FP_SIZE     3
+#define FU_INT_SIZE        3
 #define FU_FP_SIZE         1
 
-#define FU_INT_LATENCY     4
-#define FU_FP_LATENCY      9
+#define FU_INT_LATENCY     5
+#define FU_FP_LATENCY      7
 
 /* IDENTIFYING INSTRUCTIONS */
 
@@ -77,6 +77,13 @@
 
 
 /* ECE552 Assignment 3 - BEGIN CODE */
+/* DEFINES */
+#define NUM_INPUT_REGS    3
+#define NUM_OUTPUT_REGS   2
+
+/* ECE552 Assignment 3 - END CODE */
+
+/* ECE552 Assignment 3 - BEGIN CODE */
 /* VARIABLES */
 
 //instruction queue for tomasulo
@@ -110,11 +117,8 @@ typedef struct RESERVATION_STATION {
   bool              busy;
   bool              executing;
   instruction_t*    instr;
-  int               R0;
-  int               R1;
-  int               T0;
-  int               T1;
-  int               T2;
+  int               R[NUM_OUTPUT_REGS];
+  int               T[NUM_INPUT_REGS];
   int               inst_cycle;
 } res_stat_t;
 
@@ -136,6 +140,10 @@ cdb_t CDB;
 bool h_IFQ_full();
 bool h_IFQ_empty();
 void h_IFQ_push(instruction_t*);
+instruction_t* h_IFQ_head();
+instruction_t* h_IFQ_pop();
+
+void h_alloc_rs_entry(res_stat_t*, int, instruction_t*, int);
 /* ECE552 Assignment 3 - END CODE */
 
 /* ECE552 Assignment 3 - BEGIN CODE */
@@ -183,14 +191,14 @@ void CDB_To_retire(int current_cycle) {
 
   // broadcast to reservation stations
   for (int i = 0; i < RESERV_INT_SIZE; i++) {
-    if (reservINT[i].T0 == CDB.T) reservINT[i].T0 = -1;
-    if (reservINT[i].T1 == CDB.T) reservINT[i].T1 = -1;
-    if (reservINT[i].T2 == CDB.T) reservINT[i].T2 = -1;
+    for (int j = 0; j < NUM_INPUT_REGS; j++) {
+      if (reservINT[i].T[j] == CDB.T) reservINT[i].T[j] = -1;
+    }
   }
   for (int i = 0; i < RESERV_FP_SIZE; i++) {
-    if (reservFP[i].T0 == CDB.T) reservFP[i].T0 = -1;
-    if (reservFP[i].T1 == CDB.T) reservFP[i].T1 = -1;
-    if (reservFP[i].T2 == CDB.T) reservFP[i].T2 = -1;
+    for (int j = 0; j < NUM_INPUT_REGS; j++) {
+      if (reservFP[i].T[j] == CDB.T) reservFP[i].T[j] = -1;
+    }
   }
 
   // broadcast to map table
@@ -234,6 +242,7 @@ void issue_To_execute(int current_cycle) {
   /* ECE552: YOUR CODE GOES HERE */
 }
 
+/* ECE552 Assignment 3 - BEGIN CODE */
 /* 
  * Description: 
  * 	Moves instruction(s) from the dispatch stage to the issue stage
@@ -243,9 +252,60 @@ void issue_To_execute(int current_cycle) {
  * 	None
  */
 void dispatch_To_issue(int current_cycle) {
+  // if IFQ empty don't do anything
+  if (h_IFQ_empty()) {
+    printf("IFQ empty, no instrs to dispatch\n");
+    return;
+  } else {
+    printf("Dispatching...\n");
+  }
 
-  /* ECE552: YOUR CODE GOES HERE */
+  instruction_t* dispatched_instr = h_IFQ_head();
+  assert (dispatched_instr != NULL);
+  dispatched_instr->tom_dispatch_cycle++;
+
+  // conditional and unconditional branches do not get dispatched to RS
+  // so simply pop from IFQ and skip
+  if (IS_COND_CTRL(dispatched_instr->op) || IS_UNCOND_CTRL(dispatched_instr->op)) {
+    printf("dispatched instr was branch, popping from IFQ and skipping...\n");
+    h_IFQ_pop();
+    return;
+  }
+
+  // move into appropriate RS
+  bool found_available_rs_entry = false;
+  if (USES_INT_FU(dispatched_instr->op)) {
+    printf("dispatched instr uses INT FU, searching for available RS entry...\n");
+    // look for available RS
+    for (int i = 0; i < RESERV_INT_SIZE; i++) {
+      if (!reservINT[i].busy) { // this entry is not allocated!
+        // allocate this entry, then stop
+        printf("found available RS entry at index %d! allocating and popping from IFQ...\n", i);
+        h_alloc_rs_entry(&reservINT[i], i, dispatched_instr, current_cycle);
+        h_IFQ_pop();
+        printf("allocated and popped! IFQ_instr_count: %d; head: %d; tail: %d\n", IFQ_instr_count, IFQ_head, IFQ_tail);
+        found_available_rs_entry = true;
+        break;
+      }
+    }
+  } else if (USES_FP_FU(dispatched_instr->op)) {
+    printf("dispatched instr uses FP FU, searching for available RS entry...\n");
+    for (int i = 0; i < RESERV_FP_SIZE; i++) {
+      if (!reservFP[i].busy) {
+        printf("found available RS entry at index %d! allocating and popping from IFQ...\n", i);
+        h_alloc_rs_entry(&reservFP[i], i, dispatched_instr, current_cycle);
+        h_IFQ_pop();
+        printf("allocated and popped! IFQ_instr_count: %d; head: %d; tail: %d\n", IFQ_instr_count, IFQ_head, IFQ_tail);
+        found_available_rs_entry = true;
+        break;
+      }
+    }
+  } else {
+    printf("ERROR: dispatched instr uses does not INT nor FP FU\n");
+  }
+  if (!found_available_rs_entry) printf("no available RS entry.\n");
 }
+/* ECE552 Assignment 3 - END CODE */
 
 /* ECE552 Assignment 3 - BEGIN CODE */
 /* 
@@ -266,6 +326,8 @@ void fetch(instruction_trace_t* trace) {
 
   if (fetch_index <= sim_num_insn) { // only fetch if within limit of instrs
     h_IFQ_push(fetched_instr);
+  } else {
+    printf("fetch_index: %d, exceeds sim_num_insn: %d\n", fetch_index, (int)sim_num_insn);
   }
 }
 /* ECE552 Assignment 3 - END CODE */
@@ -285,6 +347,8 @@ void fetch_To_dispatch(instruction_trace_t* trace, int current_cycle) {
     printf("IFQ not full; IFQ_instr_count: %d; fetching...\n", IFQ_instr_count); // to remove
     fetch(trace);
     printf("fetched! IFQ_instr_count: %d; head: %d; tail: %d\n", IFQ_instr_count, IFQ_head, IFQ_tail); // to remove
+  } else {
+    printf("IFQ full\n");
   }
 }
 /* ECE552 Assignment 3 - END CODE */
@@ -313,11 +377,12 @@ counter_t runTomasulo(instruction_trace_t* trace)
     reservINT[i].busy         = false;
     reservINT[i].executing    = false;
     reservINT[i].instr        = NULL;
-    reservINT[i].R0           = -1;
-    reservINT[i].R1           = -1;
-    reservINT[i].T0           = -1;
-    reservINT[i].T1           = -1;
-    reservINT[i].T2           = -1;
+    for (int j = 0; j < NUM_OUTPUT_REGS; j++) {
+      reservINT[i].R[j]           = -1;
+    }
+    for (int j = 0; j < NUM_INPUT_REGS; j++) {
+      reservINT[i].T[j]           = -1;
+    }
     reservINT[i].inst_cycle   = 0;
   }
 
@@ -325,11 +390,12 @@ counter_t runTomasulo(instruction_trace_t* trace)
     reservFP[i].busy          = false;
     reservFP[i].executing     = false;
     reservFP[i].instr         = NULL;
-    reservFP[i].R0            = -1;
-    reservFP[i].R1            = -1;
-    reservFP[i].T0            = -1;
-    reservFP[i].T1            = -1;
-    reservFP[i].T2            = -1;
+    for (int j = 0; j < NUM_OUTPUT_REGS; j++) {
+      reservFP[i].R[j]           = -1;
+    }
+    for (int j = 0; j < NUM_INPUT_REGS; j++) {
+      reservFP[i].T[j]           = -1;
+    }
     reservFP[i].inst_cycle    = 0;
   }
 
@@ -371,7 +437,7 @@ counter_t runTomasulo(instruction_trace_t* trace)
     cycle++;
 
     if (is_simulation_done(sim_num_insn)) break;
-    if (cycle > 100) break; // to remove
+    if (cycle > 30) break; // to remove
   }
   
   return cycle;
@@ -402,5 +468,51 @@ void h_IFQ_push(instruction_t* instr) {
   }
   IFQ_instr_count++;
 }
+instruction_t* h_IFQ_head() {
+  return IFQ[IFQ_head];
+}
+instruction_t* h_IFQ_pop() {
+  assert(!h_IFQ_empty());
 
+  instruction_t* popped_instr;
+  popped_instr = h_IFQ_head();
+
+  IFQ[IFQ_head] = NULL;
+  if (IFQ_head == INSTR_QUEUE_SIZE-1) {
+    IFQ_head = 0;
+  } else {
+    IFQ_head++;
+  }
+  IFQ_instr_count--;
+
+  return popped_instr;
+}
+
+void h_alloc_rs_entry(res_stat_t* res_stat_entry, int res_stat_index, instruction_t* instr, int inst_cycle) {
+  res_stat_entry->busy = true;
+  res_stat_entry->executing = false;
+  res_stat_entry->instr = instr;
+
+  // T0, T1, T2: input registers
+  for (int i = 0; i < 3; i++) {
+    if (instr->r_in[i] != -1) {
+      res_stat_entry->T[i] = map_table[instr->r_in[i]];
+    } else {
+      res_stat_entry->T[i] = -1;
+    }
+  }
+
+  // R0, R1: output registers (stores don't have output registers)
+  // also update map table
+  if (!IS_STORE(instr->op)) {
+    for (int i = 0; i < 2; i++) {
+      if (instr->r_out[i] != -1) {
+        res_stat_entry->R[i] = instr->r_out[i];
+        map_table[instr->r_out[i]] = res_stat_index;
+      }
+    }
+  }
+
+  res_stat_entry->inst_cycle = inst_cycle;
+}
 /* ECE552 Assignment 3 - END CODE */
