623ca266ed2485bc9bb02d5ad12ea6ed72061364
Wu  Richard <wuricha8@ug170.eecg.toronto.edu>
2025-11-20 09:16:40 -0500

    update register numbers in mbq1.c

--- lab4/simplesim-3.0d-assig4/mbq1.c
+++ lab4/simplesim-3.0d-assig4/mbq1.c
@@ -14,23 +14,23 @@ int main(void){
 	initialize the registers to iterate over memory addresses
 */
 
+	asm("move	$2,$sp");
 	asm("move	$3,$sp");
-	asm("move	$6,$sp");
-	asm("addu	$6, $6, 12800000"); // $6 used in Scenario 2
+	asm("addu	$3, $3, 12800000"); // $6 used in Scenario 2
 
-	asm("addu	$5, $sp, 12800000"); // set the loop limits
-	asm("addu	$8, $sp, 25600000");
+	asm("addu	$4, $sp, 12800000"); // set the loop limits
+	asm("addu	$5, $sp, 25600000");
 
 /*
 	Scenario 1: memory access will jump two blocks at a time (128)
 		This goes outside next-line fetch range, so data cache miss should occur every time
 		This will iterate 100k times so should get ~100,000 dl1 cache misses here
 // */
-	asm("$L101:	sltu	$2,$3,$5");
-	asm("beq	$2,$0,$L201");
-	asm("lw		$4,0($3)");
-	asm("addu	$3,$3,128"); //increment by 2 block sizes
-	asm("j	$L101");
+	asm("$SCEN1:	sltu	$6,$2,$4");
+	asm("beq	$6,$0,$SCEN2");
+	asm("lw		$7,0($2)");
+	asm("addu	$2,$2,128"); //increment by 2 block sizes
+	asm("j	$SCEN1");
 
 /*
 	Scenario 2: memory access will jump one block at a time (64)
@@ -38,12 +38,13 @@ int main(void){
 		This loop will run 200k times so should get ~0 dl1 cache misses here
 */
   
-	asm("$L201:	sltu	$2,$6,$8");
-	asm("beq	$2,$0,$L1");
-	asm("lw		$7,0($6)");
-	asm("addu	$6,$6,64"); //increment by 1 block size
-	asm("j	$L201");
+	asm("$SCEN2:	sltu	$6,$3,$5");
+	asm("beq	$6,$0,$END");
+	asm("lw		$8,0($3)");
+	asm("addu	$3,$3,64"); //increment by 1 block size
+	asm("j	$SCEN2");
 
+	asm("$END:	nop");	
 }
 
 /*
